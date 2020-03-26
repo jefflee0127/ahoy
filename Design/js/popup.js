1736749147bfee8c9b2901a4d7410883e1102d71
Blackstar00010 <Blackstar00010@users.noreply.github.com>
2020-03-26 16:59:33 +0900

    dashboard

--- Design/js/popup.js
+++ Design/js/popup.js
@@ -3,3 +3,18 @@ function resize(obj) {
   obj.style.height = "1px";
   obj.style.height = (12+obj.scrollHeight)+"px";
 }
+
+function viewToggle(self) {
+    if (self.value==="view-replies")
+    {
+        self.innerText="Hide Replies";
+        self.value="hide-replies";
+        self.css("background-color", 'black');
+        self.html("asdfasdf");
+        $('.reply1').css('display', none);
+    }
+    else {
+        self.textContent="View Replies";
+        self.value="view-replies";
+    }
+}
