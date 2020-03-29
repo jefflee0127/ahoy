ba1b2ab94581ce35db4062d9083551dda018458d
Blackstar00010 <Blackstar00010@users.noreply.github.com>
2020-03-30 01:17:18 +0900

    notification?

---
+++ Design/js/index.js
@@ -0,0 +1,12 @@
+var options = {
+    type:"basic",
+    title:"Nardis",
+    message:"I wanna get a 36 on ACT",
+    iconUrl:"./images/get_started.png"
+}
+
+chrome.notifications.create(options, callback);
+
+function callback() {
+    
+}
