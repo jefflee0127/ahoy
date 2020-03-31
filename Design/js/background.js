000d40d5ebee641e53492262f9f81f7299f2fc74
Blackstar00010 <Blackstar00010@users.noreply.github.com>
2020-04-01 00:31:48 +0900

    5sec Notification login signup popup fixed

--- Design/js/background.js
+++ Design/js/background.js
@@ -1,24 +1,15 @@
-$('html').height($('#body').height());
-function resize(obj) {
-  obj.style.height = "1px";
-  obj.style.height = (12+obj.scrollHeight)+"px";
-}
-
-function viewToggle(asdf) {
-    alert("shit");
-    if (asdf.value==="view-replies")
-    {
-        asdf.innerText="Hide Replies";
-        asdf.value="hide-replies";
-        getElementById("content").style.display="box";
-    }
-    else {
-        asdf.textContent="View Replies";
-        asdf.value="view-replies";
-        getElementByClassName("reply1").style.display="none";
-    }
-}
-
-function replyToggle() {
-    document.getElementById("reply-or-not").style.display="box";
-}
+// setInterval(notify, 10000);
+//
+// function notify() {
+//     var option = {
+//         type: "basic",
+//         title: "Somebody",
+//         message: "Left A Comment",
+//         iconUrl: "/images/logo.png"
+//     };
+//     chrome.notifications.create(option, callback);
+//     function callback() {
+//
+//     };
+// }
+//
